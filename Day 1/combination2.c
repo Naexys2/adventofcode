37e175da460ff7adca3faad94fc1ad458a1b4bba
Naexys <nicolasblaison@yahoo.fr>
2025-12-01 11:13:12 +0100

    modified combination2.c
    hopefully it works now
    add each combination 1unit at a time and check if the movement leads to the position 0

--- Day 1/combination2.c
+++ Day 1/combination2.c
@@ -9,40 +9,38 @@ int calculate(char *fichier)
     int value;
     while (fscanf(input, "%c%d\n", &direction, &value) != EOF)
     {
-        printf("dial %d\npassword %d\n", dial, password);
         if (direction == 'L')
         {
-            while (value > 99)
+            while (value > 0)
             {
-                value -= 100;
-                password++;
-            }
-            dial -= value;
-            if (dial < 0)
-            {
-                dial += 100;
-                password++;
+                value--;
+                dial--;
+                if (dial < 0)
+                {
+                    dial = 99;
+                }
+                if (dial == 0)
+                {
+                    password++;
+                }
             }
         }
         else
         {
             while (value < 0)
             {
-                value -= 100;
-                password++;
-            }
-            dial += value;
-            if (dial > 99)
-            {
-                dial -= 100;
-                password++;
+                value--;
+                dial++;
+                if (dial > 99)
+                {
+                    dial = 0;
+                }
+                if (dial == 0)
+                {
+                    password++;
+                }
             }
         }
-        if (dial == 0)
-        {
-            password++;
-        }
-        printf("direction %c\nvalue %d\nnew dial %d\nnew password %d\n\n", direction, value, dial, password);
     }
     fclose(input);
     return password;
