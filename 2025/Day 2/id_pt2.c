92aeea11db73070bb6d1e71ae180a9737a5d6b3f
Naexys <nicolasblaison@yahoo.fr>
2025-12-02 14:00:03 +0100

    reorganized folder structure

--- 2025/Day 2/id_pt2.c
+++ 2025/Day 2/id_pt2.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-int taille_nombre(long long int nombre)
-{
-    int taille = 0;
-    while (nombre >= 1)
-    {
-        nombre = nombre / 10;
-        taille++;
-    }
-    return taille;
-}
-
 long long int add_invalid(char *fichier)
 {
     long long int password = 0;
@@ -23,15 +12,13 @@ long long int add_invalid(char *fichier)
         printf("\ninterval : %lld | %lld\n", start, stop);
         for (long long int i = start; i <= stop; i++)
         {
-            if (taille_nombre(i) % 2 == 0)
+
+            long long int part1 = i / pow(10, taille_nombre(i) / 2);
+            long long int part2 = i - part1 * pow(10, taille_nombre(i) / 2);
+            if (part1 == part2)
             {
-                long long int part1 = i / pow(10, taille_nombre(i) / 2);
-                long long int part2 = i - part1 * pow(10, taille_nombre(i) / 2);
-                if (part1 == part2)
-                {
-                    // printf("%d | %d\n", part1, part2);
-                    password += i;
-                }
+                // printf("%d | %d\n", part1, part2);
+                password += i;
             }
         }
     }
