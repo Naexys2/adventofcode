8ab086231df6eb09f8275a7a6a3eaa0102d9518e
Naexys <nicolasblaison@yahoo.fr>
2025-12-02 14:16:50 +0100

    added function in pt2
    still WIP

--- 2025/Day 2/id_pt2.c
+++ 2025/Day 2/id_pt2.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
+int taille_nombre(long long int nombre)
+{
+    int taille = 0;
+    while (nombre >= 1)
+    {
+        nombre = nombre / 10;
+        taille++;
+    }
+    return taille;
+}
+
+int est_diviseur(int a, int b)
+{
+    if (b % a == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int nombre_diviseur(int taille)
+{
+    int nb_div = 0;
+    for (int i = 1; i <= taille; i++)
+    {
+        if (est_diviseur(i, taille))
+        {
+            nb_div++;
+        }
+    }
+    return nb_div;
+}
+
 long long int add_invalid(char *fichier)
 {
     long long int password = 0;
@@ -9,15 +42,12 @@ long long int add_invalid(char *fichier)
     long long int stop;
     while (fscanf(input, "%lld-%lld,", &start, &stop) != EOF)
     {
-        printf("\ninterval : %lld | %lld\n", start, stop);
         for (long long int i = start; i <= stop; i++)
         {
-
             long long int part1 = i / pow(10, taille_nombre(i) / 2);
             long long int part2 = i - part1 * pow(10, taille_nombre(i) / 2);
             if (part1 == part2)
             {
-                // printf("%d | %d\n", part1, part2);
                 password += i;
             }
         }
